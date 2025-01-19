1572ef4bccff6c371c34622a4f15653653e8138e
Segal G <77511272+Sxgvl@users.noreply.github.com>
2025-01-19 20:41:40 +0100

    Initial Commit

---
+++ little-x-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  little-x-project
+//
+//  Created by Segal GBENOU on 19/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ little-x-project/little_x_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_x_projectApp.swift
+//  little-x-project
+//
+//  Created by Segal GBENOU on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct little_x_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
